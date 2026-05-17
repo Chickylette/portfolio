d4cd6c2ee56ce20f8e48a14cd5936026929c7ace
Chickylette <jjgundam315@gmail.com>
2026-05-17 23:03:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    // Get elements
     const sideMenu = document.getElementById("sideMenu");
     const menuButton = document.getElementById("menuButton");
 
+    // Open / Close menu when button is clicked
     menuButton.addEventListener("click", function (event) {
+
+        // Prevent click from immediately closing menu
         event.stopPropagation();
+
+        // Toggle menu
         sideMenu.classList.toggle("open");
     });
 
-    document.addEventListener("click", function (event) {
-        const clickedInsideMenu = sideMenu.contains(event.target);
-        const clickedMenuButton = menuButton.contains(event.target);
+    // Prevent menu clicks from closing menu
+    sideMenu.addEventListener("click", function (event) {
+        event.stopPropagation();
+    });
 
-        if (!clickedInsideMenu && !clickedMenuButton) {
+    // Close menu when clicking anywhere outside
+    document.addEventListener("click", function () {
+
+        // Close menu only if open
+        if (sideMenu.classList.contains("open")) {
             sideMenu.classList.remove("open");
         }
     });
+
 });
